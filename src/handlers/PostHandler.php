5c51eab866d8928cfdeef1a87e8d71b3a0c4842c
Gabriel Albuquerque <contato@gabtech.com.br>
2021-07-05 19:26:56 -0300

    Começando feed

--- src/handlers/PostHandler.php
+++ src/handlers/PostHandler.php
@@ -2,6 +2,8 @@
 namespace src\handlers;
 
 use \src\models\Post;
+use \src\models\User;
+use \src\models\UserRelation;
 
 class PostHandler {
 
@@ -18,4 +20,50 @@ class PostHandler {
       ])->execute();
     }
   }
+
+  public static function getHomeFeed($idUser) {
+
+    // 1 lista de ususarios que eu sigo
+    $userList = UserRelation::select()->where('user_from', $idUser)->get();
+    $users = [];
+    foreach($userList as $userItem) {
+      $users[] = $userItem['user_to'];
+    }
+    $users[] = $idUser;
+
+    // 2 pegar os posts dessa galera ordenado pela data
+    $postList = Post::select()
+    ->where('id_user', 'in', $users)
+    ->orderBy('created_at', 'desc')
+    ->get();
+
+    // 3 transormar o resultado em objetos dos models de
+    $posts = [];
+    foreach($postList as $postItem) {
+      $newPost = new Post();
+      $newPost->id = $postItem['id'];
+      $newPost->type = $postItem['type'];
+      $newPost->created_at = $postItem['created_at'];
+      $newPost->body = $postItem['body'];
+
+      // 4 preencher as informações adicionais
+      $newUser = User::select()
+      ->where('id', $postItem['id_user'])
+      ->one();
+
+      $newPost->user = new User();
+      $newPost->user->id = $newUser['id'];
+      $newPost->user->name = $newUser['name'];
+      $newPost->user->avatar = $newUser['avatar'];
+
+      // 4.1 preencher informações de LIKE
+      // 4.2 preencher informações do COMMENTS
+
+      $posts[] = $newPost;
+      
+    }
+    // 5 retornar
+    return $posts;
+  }
+  
 }
